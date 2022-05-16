d2beb0cf8984cd9b9db5d7952c980dea52611c1e
pardub <42610272+pardub@users.noreply.github.com>
2022-05-16 22:22:57 +0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,4 +1,5 @@
 # Install pluging zsh-syntax-highlighting
 cd /usr/share/zsh/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "# Load zsh-syntax-highlighting; should be last." >> $ZDOTDIR/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null" >> $ZDOTDIR/.zshrc
