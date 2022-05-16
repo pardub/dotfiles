923c617bd994b4535658bcd511c56148cc6905b1
pardub <42610272+pardub@users.noreply.github.com>
2022-05-16 23:31:26 +0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -3,3 +3,9 @@ cd /usr/share/zsh/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "# Load zsh-syntax-highlighting; should be last." >> $ZDOTDIR/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null" >> $ZDOTDIR/.zshrc
+
+# Install pluging zsh-autosuggestions
+cd /usr/share/zsh/plugins/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+echo "# Load zsh-autosuggestions; should be last." >> $ZDOTDIR/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 2>/dev/null" >> $ZDOTDIR/.zshrc
