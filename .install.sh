2e15343b1975c330ca481871e80a7db4dcf071d5
pardub <42610272+pardub@users.noreply.github.com>
2022-05-17 14:16:42 +0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -3,8 +3,8 @@
 sudo dnf -y update
 
 ## Setup Visudo
-echo 'marc ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
-echo 'Defaults:marc timestamp_timeout=60' | sudo EDITOR='tee -a' visudo
+echo "$USER ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
+echo "Defaults:$USER timestamp_timeout=60" | sudo EDITOR='tee -a' visudo
 
 # ADD EXTRA REPOS rpm fusion
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
