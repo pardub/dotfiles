c61ff5552652942b1aa2f4310e491711ed576e6e
pardub <42610272+pardub@users.noreply.github.com>
2022-05-30 19:05:00 +0300

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh -xev
 
 # Install pluging zsh-syntax-highlighting
-sudo mkdir -p "$ZDOTDIR"/plugins || exit
-cd "$ZDOTDIR"/plugins || exit
+mkdir -p "$ZDOTDIR/plugins" || exit
+cd "$ZDOTDIR/plugins" || exit
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # Install pluging zsh-autosuggestions
-cd "$ZDOTDIR"/plugins || exit
+cd "$ZDOTDIR/plugins" || exit
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
 sudo reboot
