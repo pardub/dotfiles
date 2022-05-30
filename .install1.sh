adf6db9b6654f1168458b1eca5b8f38a0cdb8d21
pardub <42610272+pardub@users.noreply.github.com>
2022-05-30 16:46:59 +0300

    Update .install1.sh

--- .install1.sh
+++ .install1.sh
@@ -300,7 +300,7 @@ sudo systemctl enable iptables
 # FIREWALL SET UP OPEN/CLOSED PORTS
 
 # VS CODE INSTALL EXTENSIONS
-#code --install-extension ginfuru.ginfuru-better-solarized-dark-theme
+code --install-extension ginfuru.ginfuru-better-solarized-dark-theme-0.9.5
 #code --install-extension ms-azuretools.vscode-docker	
 #code --install-extension dbaeumer.vscode-eslint
 #code --install-extension redhat.vscode-yaml
