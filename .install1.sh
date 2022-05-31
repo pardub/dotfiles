ed4b18e25c5a46c0f8c2d8891222053409dc6a05
pardub <42610272+pardub@users.noreply.github.com>
2022-05-31 20:16:26 +0300

    Update .install1.sh

--- .install1.sh
+++ .install1.sh
@@ -21,8 +21,8 @@ sudo dnf -y update
 # INSTALL JETBRAINS MONO FONTS IN ~/.local/share/fonts
 mkdir -p "$HOME"/.local/share/fonts/nerd-fonts
 cd /tmp
-#sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+#sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
 sudo unzip /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/nerd-fonts
 fc-cache -f -v
 cd || exit
