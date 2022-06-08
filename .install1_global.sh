2b626a8cc39e02f3b6ed0099e220c856904d213c
pardub <42610272+pardub@users.noreply.github.com>
2022-06-08 23:20:20 +0300

    Update .install1_global.sh

--- .install1_global.sh
+++ .install1_global.sh
@@ -341,8 +341,8 @@ gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profi
 # OPENSNITCH
 sudo mkdir -p /opt/opensnitch || exit
 cd /opt/opensnitch || exit
-wget https://github.com/evilsocket/opensnitch/releases/download/v1.5.0/opensnitch-1.5.0-1.x86_64.rpm
-wget https://github.com/evilsocket/opensnitch/releases/download/v1.5.0/opensnitch-ui-1.5.0-1.noarch.f29.rpm
+sudo wget https://github.com/evilsocket/opensnitch/releases/download/v1.5.0/opensnitch-1.5.0-1.x86_64.rpm
+sudo wget https://github.com/evilsocket/opensnitch/releases/download/v1.5.0/opensnitch-ui-1.5.0-1.noarch.f29.rpm
 sudo dnf -y localinstall opensnitch-1*.rpm; sudo dnf -y localinstall opensnitch-ui*.rpm
 sudo systemctl enable --now opensnitch || exit
 sudo systemctl start opensnitch || exit
