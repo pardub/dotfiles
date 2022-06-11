05a4ba3fac52286bcefbd5891b4becf9d7216f1e
pardub <42610272+pardub@users.noreply.github.com>
2022-06-11 13:32:52 +0300

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,16 +1,24 @@
 #!/bin/zsh
  
 ###*** FIREWALL FOR A DESKTOP OPERATING SYSTEM ***###
+
+# Flushing all iptables tables
+iptables -t filter -F
+iptables -t raw -F
+iptables -t nat -F
+iptables -t mangle -F
+iptables -X
  
-#flush filter table from all chains
-iptables -F 
+# Flushing and destroying all ip sets
+ipset -F
+ipset -X
  
-#allow loopback interface traffic 
+# Allow loopback interface traffic 
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
  
  
-#drop invalid packets on INPUT and OUTPUT CHAINS
+# Drop invalid packets on INPUT and OUTPUT CHAINS
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A OUTPUT -m state --state INVALID -j DROP
  
@@ -21,13 +29,13 @@ iptables -A OUTPUT -m state --state INVALID -j DROP
 #optional, uncomment the line if you want to allow incoming ssh connection from a specific ip address
 #iptables -A INPUT -p tcp --dport 22 --syn -s 80.0.0.1 -j ACCEPT
  
-#allow only ESTABLISHED and RELATED packets on INPUT
+# Allow only ESTABLISHED and RELATED packets on INPUT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
  
-#allow also NEW packets on OUTPUT (packets that initialize connections)
+# Allow also NEW packets on OUTPUT (packets that initialize connections)
 iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
  
  
-#set policy to DROP on INPUT and OUTPUT CHAINS
+# Set policy to DROP on INPUT and OUTPUT CHAINS
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
