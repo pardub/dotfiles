17d777f4fdaa0a75b58f66f8476d1f9103b99e4d
pardub <42610272+pardub@users.noreply.github.com>
2022-06-27 14:42:03 +0100

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -4,12 +4,12 @@
 //user_pref("browser.safebrowsing.malware.enabled", false);
 //user_pref("browser.safebrowsing.phishing.enabled", false);
 
-//* 0402: disable SB checks for downloads (both local lookups + remote)
+//* 0402: Enable SB checks for downloads (both local lookups + remote)
 //user_pref("browser.safebrowsing.downloads.enabled", false);
 
 //* 0403: disable SB checks for downloads (remote)
 
-user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
 
 //* 0404: disable SB checks for unwanted software
 //user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
@@ -28,7 +28,7 @@ user_pref("browser.send_pings", false); // [DEFAULT: false]
 
 //* [SETTING] General>Tabs>Enable Container Tabs>Settings>Select a container for each new tab ***/
 
-user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
+//user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
 
 
 //*** [SECTION 2000]: PLUGINS / MEDIA / WEBRTC ***/
@@ -97,5 +97,33 @@ user_pref("browser.startup.homepage", "https://search.dr460nf1r3.org/");
 //user_pref("browser.newtabpage.enabled", true);
 //user_pref("browser.newtab.preload", true);
 
+//* 1004: set the minimum interval between session save operations
+// [UNBREAK=(default value)] interval in seconds at which session data is stored (restore session after browser crash) - '9999999' essentially disables session store to reduce disk writes - previous opened tabs will still be restored on startup (see also: 'browser.startup.page')
+user_pref("browser.sessionstore.interval", 9999999);
 
 
+/* UPDATES ***/
+// [NON-WINDOWS] disable auto app updates
+user_pref("app.update.auto", false);
+
+// Minimize tab size 
+user_pref("browser.tabs.tabMinWidth", 38);
+
+/**
+ * [SET] the following preferences adjusts the smooth scrolling feature of
+ * Firefox when using a mouse wheel or keyboard keys to scroll
+ */
+user_pref("general.smoothScroll.lines.durationMaxMS", 400);         // smooth the start/end of line scrolling operations in ms (up/down arrow/page keys)
+user_pref("general.smoothScroll.lines.durationMinMS", 200);         // smooth the start/end of line scrolling operations in ms (up/down arrow/page keys)
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 600);    // smooth the start/end of scrolling operations in ms
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 300);    // smooth the start/end of scrolling operations in ms
+user_pref("general.smoothScroll.other.durationMaxMS", 400);         // smooth the start/end of other scrolling operations in ms
+user_pref("general.smoothScroll.other.durationMinMS", 200);         // smooth the start/end of other scrolling operations in ms
+user_pref("general.smoothScroll.pages.durationMaxMS", 400);         // smooth the start/end of page scrolling operations in ms (PgUp/PgDn keys)
+user_pref("general.smoothScroll.pages.durationMinMS", 200);         // smooth the start/end of page scrolling operations in ms (PgUp/PgDn keys)
+user_pref("mousewheel.acceleration.factor", 10);                    // sets acceleration factor if mouse wheel.acceleration.start > -1
+user_pref("mousewheel.acceleration.start", 0);                      // when to apply mouse wheel.acceleration.factor (after how many scroll clicks of mouse wheel) - value must be greater than -1
+user_pref("mousewheel.default.delta_multiplier_x", 85);             // sets the x-axis step size
+user_pref("mousewheel.default.delta_multiplier_y", 85);             // sets the y-axis step size
+user_pref("mousewheel.default.delta_multiplier_z", 85);             // sets the z-axis step size
+user_pref("mousewheel.min_line_scroll_amount", 10);                 // if the CSS line height is smaller than this value in pixels, each scroll click will scroll this amount
