11b4005f345b81ab586f349616c5a574e0137123
pardub <42610272+pardub@users.noreply.github.com>
2022-08-25 18:22:50 +0000

    Update install_1_global.sh

--- install_1_global.sh
+++ install_1_global.sh
@@ -259,9 +259,6 @@ sudo systemctl start libvirtd
 sudo systemctl enable libvirtd
 sudo dnf -y install gnome-boxes
 
-sudo systemctl start libvirtd
-sudo systemctl enable libvirtd
-
 # +-------------------------------+
 # | Manual install - Intellj Idea |
 # +-------------------------------+
