5e56895208f8cc75377431c40dbfd584c0f3d548
pardub <42610272+pardub@users.noreply.github.com>
2022-09-01 08:47:57 +0000

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -8,6 +8,9 @@
 echo "$USER ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
 echo "Defaults:$USER timestamp_timeout=60" | sudo EDITOR='tee -a' visudo
 
+# Add dns from https://dns.watch/
+sudo echo 'nameserver 84.200.69.80' | sudo tee -a /etc/resolv.conf
+sudo echo 'nameserver 84.200.70.40' | sudo tee -a /etc/resolv.conf
 
 mkdir -p "$HOME"/.config/zsh
 cd || exit
@@ -17,5 +20,6 @@ sudo echo 'deltarpm=True'                  | sudo tee -a /etc/dnf/dnf.conf
 sudo echo 'max_parallel_downloads=10'      | sudo tee -a /etc/dnf/dnf.conf
 sudo sudo dnf clean all
 sudo dnf -y update
+
 
 # sudo reboot
