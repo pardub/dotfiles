3e55af723c2f180e764902f27ee7bff9621d24f3
pardub <42610272+pardub@users.noreply.github.com>
2022-12-19 18:40:36 +0000

    Update install_2_global.sh

--- install_2_global.sh
+++ install_2_global.sh
@@ -589,7 +589,7 @@ sudo sed -i 's/SELINUX=disabled/SELINUX=enforcing/' /etc/selinux/config
 # INSTALL JETBRAINS MONO FONTS IN ~/.local/share/fonts
 mkdir -p "$HOME"/.local/share/fonts/nerd-fonts
 cd /tmp
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
 #sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
 sudo unzip /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/nerd-fonts
 fc-cache -v
