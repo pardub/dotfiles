b3fb0393ee11646ef06f6cff0bc75e1d159f59a8
pardub <42610272+pardub@users.noreply.github.com>
2023-04-08 13:08:48 +0000

    Create .emacs-profles.el

---
+++ dotfiles/.emacs-profles.el
@@ -0,0 +1,3 @@
+(("myemacs" .  ((user-emacs-directory     . "~/.config/dotfiles/myemacs")))
+("spacemacs" . ((user-emacs-directory     . "~/.config/dotfiles/spacemacs")))
+("doomemacs" . ((user-emacs-directory     . "~/.config/dotfiles/doomemacs"))))
